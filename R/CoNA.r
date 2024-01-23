dae8d2760f27a1b8bc5ffe41c6b6800e2a7b160e
Juan Camilo Ordoñez Deñas <foodprice2024@gmail.com>
2024-01-23 17:48:15 -0500

    Finalización precios sombra mod 2

--- R/CoNA.r
+++ R/CoNA.r
@@ -16,7 +16,7 @@ if (!require("pacman")) install.packages("pacman") # Paquete que simplifica la c
 pacman::p_load(char = Librerias_base);Librerias_base_print = paste0(paste0("'", Librerias_base, "'"), collapse = ", ") # Instala si es necesario, o en su defecto, sólo llama los paquetes
 
 cat("\n")
-print("Se instalaron y cargaron todas la librerias corectamente")
+cat("Se instalaron y cargaron todas la librerias corectamente")
 cat("\n")
 
 #------------------------------------------------------------------------------------------#
@@ -125,7 +125,7 @@ DRI_max_li= DRI_max_i %>% select(-any_of(c("Edad","Energia","Sexo")))
 DF_Nutrientes_ALimentos <- Datos_Insumo %>% select(-any_of(c("Cod_TCAC", "Alimento", "Serving", "Precio_100g_ajust")))
 
 # Identificar las columnas que son iguales de datos insumo y requerimientos y ordenarlas
-nombres_comunes <- intersect(names(DF_Nutrientes_ALimentos), names(DRI_min_li))
+nombres_comunes <- intersect(names(DF_Nutrientes_ALimentos), names(DRI_min_li));
 
 # Ordenar los nombrespara el modelo
 DF_Nutrientes_ALimentos <- DF_Nutrientes_ALimentos %>% select(any_of(nombres_comunes));DRI_min_li <- DRI_min_li %>% select(any_of(nombres_comunes));  DRI_max_li <- DRI_max_li %>% select(any_of(nombres_comunes))
@@ -139,7 +139,7 @@ DF_Nutrientes_ALimentos=cbind(DF_Nutrientes_ALimentos,Sin_EER)
 
 # Matriz de coef de restricción al modelo (ENERGIA y nutrientes)
 Coef.Restriq=DF_Nutrientes_ALimentos %>% as.matrix() %>% t()
-nrow(Coef.Restriq)
+
 
 #signos de las restricciones
 constr_signs = c("=", rep(">=", ncol(DRI_min_li)-1), rep("<=", length(DRI_max_li)))
@@ -156,10 +156,15 @@ Intercambios_CoNA <- data.frame(Alimento = character(), Cantidad_GR = numeric(),
 #DF de la solución de csotos
 Costo_T <- data.frame(Grupo_demo = integer(), Sexo = integer(), Costo_dia = numeric())
 
+# df de nutrientes limitantes
+N_limit=data.frame()
 
+# DF de los precios sombra
+S_shadow = na.omit(data.frame(Edad = NA,Sexo = NA,Nutrientes = NA,constraint = NA,value_constraint = NA,SP = NA, SPE = NA))
 # ------------ -------------------- SOLUCIÓN DEL MODELO
 
 for (i in seq_along(Edad)) { #ciclo para cada edad
+
 
 CoNA = lp(direction = "min",
                objective.in = Precio,
@@ -194,46 +199,112 @@ Costo_dia=sum(CoNA$solution * Precio)
 # Agregar los resultados al dataframe Costo_CoNA
 temp_df <- data.frame(Grupo_demo = Edad[i],
                           Sexo = as.numeric(sexo_nombre),
-                          Costo_dia = sum(CoNA$solution * Precio))
+                          Costo_dia = sum(CoNA$solution * Precio),
+                          Costo_1000kcal=(CoNA$objval/as.vector(unlist(Limitaciones[i, , drop = FALSE])[1])*1000))
 
 # Agregar los resultados al dataframe general
 
 Costo_T=rbind(Costo_T, temp_df)
 
+  #------------------------------------#
+  #       NUTRIENTES LIMITANTES        #
+  #------------------------------------#
 
-}
+
+  Nutrie_limit <- data.frame(
+    Nutrientes = rownames(Coef.Restriq[1:length(names(DRI_min_li)),] %*% as.matrix(CoNA$solution)), # se asumen nutrientes sin EER y sin repetir
+    Opt = as.numeric(Coef.Restriq[1:length(names(DRI_min_li)),] %*% as.matrix(CoNA$solution))
+  ) %>%  
+    mutate(Rest = as.matrix(as.vector(unlist(DRI_min_li[i, , drop = FALSE]))),
+           Diff = round(((Opt - Rest) / Rest * 100),2),
+           Limiting = ifelse(Diff == 0, 1, 0),
+           Edad = Edad[i],
+           Sexo = as.numeric(sexo_nombre)) %>% filter(Nutrientes != "Energia") 
+  N_limit = rbind(N_limit, Nutrie_limit)
+
+  #------------------------------------#
+  #       PRECIOS SOMBRA Y ELAST       #
+  #------------------------------------#
+
+ Spe <- data.frame(
+    Edad = Edad[i],
+    Sexo = as.numeric(sexo_nombre),
+    Nutrientes = names(Limitaciones),
+    constraint = constr_signs,
+    value_constraint = as.vector(unlist(Limitaciones[i, , drop = FALSE])),
+    SP = NA,
+    SPE = NA
+  ) %>%
+    mutate(SP = CoNA$duals[1:length(constr_signs)],
+           SPE = ((SP/1) * (unlist(Limitaciones[i, , drop = FALSE])/CoNA$objval)) * 100)
+  # Añadir a output4
+  S_shadow <- rbind(S_shadow, Spe)
+  
+
+} #FIN DEL CICLO EN EDAD
+
 
 # Asignaciones por sexo
 assign(paste("CoNA_", sexo_nombre, sep = ""), Costo_T)
 assign(paste("Intercambios_CoNA_", sexo_nombre, sep = ""), Intercambios_CoNA)
 
+# asginaciones nutrientes limitantes por sexo
+assign(paste("N_limit_", sexo_nombre, sep = ""), N_limit)
+
+# asignaicoens precios sombra por sexo
+assign(paste("S_shadow_", sexo_nombre, sep = ""), S_shadow)
 #--------------------------------------------------------#
 #        FIND DEL       CLICLO PARA CADA SEXO            #
 #-------------------------------------------------------#
 
 }
 
+
+nombres_comunes_sin_energia <- setdiff(nombres_comunes, "Energia")
+cat("\n")
+cat("Los nutrientes a usar en el modelo son:", paste(nombres_comunes_sin_energia, collapse = ", "), "\n")
+cat("\n")
+
 # Unir ambos df para cada sexo (si existe)
 if ("Sexo" %in% colnames(DRI_min)) {
 
 Costo_CoNA=rbind(CoNA_0,CoNA_1)
 Alimentos_CoNA=rbind(Intercambios_CoNA_0,Intercambios_CoNA_1)
-  
+CoNA_N_Limit=rbind(N_limit_0,N_limit_1) 
+CoNA_SP=rbind(S_shadow_0,S_shadow_1);CoNA_SP = CoNA_SP %>% filter(constraint == ">=")
+CoNA_SP = CoNA_SP[c("Edad", "Sexo", "Nutrientes", "SP", "SPE")]
   } else {
+
    Costo_CoNA <- CoNA_0 %>%
   select(-Sexo)
   Alimentos_CoNA<- Intercambios_CoNA_0 %>%
   select(-Sexo)
+  CoNA_N_Limit<- N_limit_0 %>%
+  select(-Sexo)
+    CoNA_SP<- S_shadow_0 %>%
+  select(-Sexo)
+
+  CoNA_SP = CoNA_SP %>% filter(constraint == ">=")
+CoNA_SP = CoNA_SP[c("Edad", "Nutrientes", "SP", "SPE")]
 }
+
+#Nutrientes limtantes y precios sombre
+CoNA_SP_LM = merge(CoNA_SP, CoNA_N_Limit,by = c("Edad", "Sexo", "Nutrientes"))
 
 # Asignación en el ambiente global
 assign("Costo_CoNA",Costo_CoNA,envir = globalenv()) 
 assign("Alimentos_CoNA",Alimentos_CoNA,envir = globalenv()) 
+assign("CoNA_SP_LM",CoNA_SP_LM,envir = globalenv()) 
 
 #------------------------------------------------------------------------------------------#
 #                       FIN DEL TERCER MÓDULO COMO FUNCIÓN                               #
 #-----------------------------------------------------------------------------------------#
-print ("Ejecución del modelo: 'COSTO DIARIO A UNA DIETA ADECUADA EN NUTRIENTES (CoNA)' correcta") 
+cat("\n")
+cat("Ejecución del modelo: 'COSTO DIARIO A UNA DIETA ADECUADA EN NUTRIENTES (CoNA)' correcta") 
+cat("\n")
 
 }
+
+
+
 
