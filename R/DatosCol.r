5024d0f2b135ee52e62f2b27f1c4a76ab9781e8e
sergiobarona03 <115027994+sergiobarona03@users.noreply.github.com>
2024-01-24 12:34:19 -0500

    Update DatosCol.r

--- R/DatosCol.r
+++ R/DatosCol.r
@@ -887,10 +887,10 @@ Datos_MOD3 <- Datos_MOD3 %>%
 #                       ASGINACIÓN EN EL ENTORNO GLOBAL                                   #
 #-----------------------------------------------------------------------------------------#
 
-assign(paste0("Datos_",Año,"_",Mes_Num,"_",Ciudad),Datos_MOD3,envir = globalenv())
+#assign(paste0("Datos_",Año,"_",Mes_Num,"_",Ciudad),Datos_MOD3,envir = globalenv())
 
 print(paste("✔_",Ciudad,"_" , Año,"_" ,Mes,""))
-
+return(invisible(Datos_MOD3))
 
 #cat("\n")
 # if(length(warnings())<100) {cat("Depuración de datos exitosa", "\n")} else {cat("Cantidad de errores encontrados:",length(warnings()), "\n")}
