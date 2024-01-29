73f7977228a2011f2dad2f5b00178883584ddddb
sergiobarona03 <115027994+sergiobarona03@users.noreply.github.com>
2024-01-29 16:35:31 -0500

    Update CoNA.r

--- R/CoNA.r
+++ R/CoNA.r
@@ -344,7 +344,7 @@ CoNA=function(Datos_Insumo,DRI_min,DRI_max,Filtrar_Alimentos=NULL){
   
   nombres_comunes_sin_energia <- setdiff(nombres_comunes, "Energia")
   cat("\n")
-  cat("Los nutrientes a usar en el modelo son:", paste(nombres_comunes_sin_energia, collapse = ", "), "\n")
+ # cat("Los nutrientes a usar en el modelo son:", paste(nombres_comunes_sin_energia, collapse = ", "), "\n")
   cat("\n")
   
   # Unir ambos df para cada sexo (si existe)
