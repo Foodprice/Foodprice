470c88a096b44bbe8376a9210d9b8d6000594762
Juan Camilo Ordoñez Deñas <foodprice2024@gmail.com>
2024-02-12 16:26:54 -0500

    Eliminando pauqte SOlve

--- R/DataCol.r
+++ R/DataCol.r
@@ -82,7 +82,7 @@ if (!is.null(Margins)) {
   #-----------------------------------------------------------------------------------------#
   
   Librerias_base = c("readxl","dplyr","ggplot2","reshape2","knitr","haven","foreign","stringi","labelled","tidyr","plyr","tidyverse",
-                     ,"Rglpk","scatterplot3d","reshape","R6","rio","janitor","lubridate","stringr","stringdist") # Nombra las librerias necesarias
+                     "Rglpk","scatterplot3d","reshape","R6","rio","janitor","lubridate","stringr","stringdist") # Nombra las librerias necesarias
   
   if (!require("pacman")) install.packages("pacman") # Paquete que simplifica la carga de librerias
   pacman::p_load(char = Librerias_base,character.only = TRUE);Librerias_base_print = paste0(paste0("'", Librerias_base, "'"), collapse = ", ") # Instala si es necesario, o en su defecto, sólo llama los paquetes
